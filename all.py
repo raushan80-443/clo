70190d6df0e4da1a9b4be7fc70c35840e7e3f20f
Raushan Kumar <raushan102188@gmail.com>
2023-11-11 23:24:01 +0530

    clear cap files

--- all.py
+++
@@ -1,8 +0,0 @@
-import os
-# a = os.listdir()
-a = os.listdir()
-print(a)
-for c in a: 
-    if ".txt" in c:
-        with open("all","a") as wri:
-            wri.write(f"hashcat -m 22000 -w 4 -d 1  test.22000 {c} \n")
